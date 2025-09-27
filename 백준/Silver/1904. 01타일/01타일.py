b5f64d8183ea7f4ecd690359c941851bf4ce5ded
kim Yong Hwan <74572506+gyeomfka@users.noreply.github.com>
2025-09-27 13:24:57 +0900

    [Silver III] Title: 01타일, Time: 224 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/1904. 01타일/01타일.py
@@ -0,0 +1,13 @@
+n = int(input())
+
+MOD = 15746
+
+if n == 1:
+    print(1)
+elif n == 2:
+    print(2)
+else:
+    a, b = 1, 2  # f(1), f(2)
+    for _ in range(3, n + 1):
+        a, b = b, (a + b) % MOD
+    print(b)
